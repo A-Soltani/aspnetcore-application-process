6fe4a335c5bb75d29cf9320c704123d586d21d8c
Ali Soltani <soltani1384@gmail.com>
2021-01-23 23:01:59 +0330

    adding Applicant and Address

---
+++ src/services/ApplicationProcessing/Hahn.ApplicationProcess.December2020.Domain/AggregatesModel/ApplicantAggregate/Applicant.cs
@@ -0,0 +1,16 @@
+﻿using Hahn.ApplicationProcess.December2020.Domain.SeedWork;
+
+namespace Hahn.ApplicationProcess.December2020.Domain.AggregatesModel.ApplicantAggregate
+{
+    public class Applicant : Entity, IAggregateRoot
+    {
+        public string Name { get; private set; }
+        public string FamilyName { get; private set; }
+        public Address Address { get; private set; }
+        public string CountryOfOrigin { get; private set; }
+        public string EmailAddress { get; private set; }
+        public int Age { get; private set; }
+        public bool Hired { get; private set; }
+
+    }
+}
