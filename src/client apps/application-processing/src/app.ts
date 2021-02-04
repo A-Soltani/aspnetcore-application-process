3ee960a95f076a54af8a9e42fb01e2f91e61f1de
Ali Soltani <soltani1384@gmail.com>
2021-02-04 16:55:28 +0330

    adding header and footer

--- src/client apps/application-processing/src/app.ts
+++ src/client apps/application-processing/src/app.ts
@@ -1,3 +1,27 @@
+import { Router, RouterConfiguration } from 'aurelia-router';
+import { PLATFORM } from 'aurelia-pal';
+
 export class App {
-  public message = 'Hello World!';
+  router: Router;
+
+  // configureRouter(config: RouterConfiguration, router: Router){
+  //   config.title = 'Contacts';
+  //   config.options.pushState = true;
+  //   config.options.root = '/';
+  //   config.map([
+  //     { route: ['', 'home'], name: 'home', moduleId: PLATFORM.moduleName('applicants/applicant-list'), nav: true, title: 'Home' },
+  //     { route: ['applicants', 'applicants'], name: 'applicants', moduleId: PLATFORM.moduleName('applicants/applicant-list'), nav: true, title: 'Home' },
+
+  //   ]);
+
+  //   this.router = router;
+  // }
+  configureRouter(config, router): void {
+    config.title = 'Aurelia';
+    config.map([
+      { route: ['', 'home'], name: 'home', moduleId: PLATFORM.moduleName('home/home'), nav: true, title: 'Home' },
+    ]);
+    this.router = router;
+  }
 }
+
